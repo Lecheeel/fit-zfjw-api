9fb270122215578e9d7e0f5ad13089974a49d0b9
Lecheeel <ryancuee@gmail.com>
2024-04-02 15:06:32 +0800

    Refactor test_schedule_manager.py

--- test_schedule_manager.py
+++ test_schedule_manager.py
@@ -6,8 +6,8 @@ class TestScheduleManager(unittest.TestCase):
     def setUp(self):
         self.manager = ScheduleManager("test_schedule.json")
 
-    def test_load_schedule_from_json(self):
-        schedule = self.manager.load_schedule_from_json("test_schedule.json")
+    def test_load_json(self):
+        schedule = self.manager.load_json("test_schedule.json")
         self.assertIsInstance(schedule, list)
         for course in schedule:
             self.assertIsInstance(course, Course)
@@ -17,14 +17,6 @@ class TestScheduleManager(unittest.TestCase):
         self.assertIsInstance(courses, list)
         for course in courses:
             self.assertIsInstance(course, Course)
-
-    def test_check_schedule_at_time(self):
-        current_course, next_courses = self.manager.check_schedule_at_time(datetime.datetime.now())
-        if current_course:
-            self.assertIsInstance(current_course, Course)
-        if next_courses:
-            for course in next_courses:
-                self.assertIsInstance(course, Course)
 
 if __name__ == "__main__":
     unittest.main()
